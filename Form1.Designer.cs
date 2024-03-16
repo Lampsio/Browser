7263b907e2d7e8e688c8fe4f187fbecf1d792c07
Daniel <daniels47@poczta.fm>
2024-03-16 20:47:33 +0100

    browser

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace PrzegladarkaProjekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(46, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(114, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Odśwież";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(195, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(668, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(869, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 35);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Idz";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(950, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 35);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Nowy";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 53);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1407, 509);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1399, 480);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1032, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 34);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Historia";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1127, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(182, 34);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Wyczysc Historie";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1431, 565);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Przegladarka";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using CefSharp.WinForms;
+using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrzegladarkaProjekt
+{
+    public partial class Form1 : Form
+    {
+        private DataTable dataTable;
+        private int currentId;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ChromiumWebBrowser web;
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            CefSettings settings = new CefSettings(); //ustawienia dla biblioteki CefSharp
+            //Inicjalizacja
+            Cef.Initialize(settings);
+            textBox1.Text = "https://www.interia.pl";
+            web = new ChromiumWebBrowser(textBox1.Text);
+            web.Parent = tabControl1.SelectedTab; //wyswietlenie nazwy w zakladce
+            //this.panel1.Controls.Add(web);
+            web.Dock = DockStyle.Fill; //wypelnia calą dostpena
+            web.AddressChanged += web_AddressChanged;
+            web.TitleChanged += web_TitleChanged;
+            
+
+            // Tworzenie tabeli
+            dataTable = new DataTable();  
+            dataTable.Columns.Add("ID", typeof(int));
+            dataTable.Columns.Add("Godzina", typeof(DateTime));
+            dataTable.Columns.Add("LinkDoStrony", typeof(string));
+
+            AddDate();
+        }
+
+        //zmiana adresu
+        private void web_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+                {
+                textBox1.Text = e.Address;
+                    AddDate();
+            }));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser web = tabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if (web != null)
+            {
+                String url = textBox1.Text;
+                if (Uri.TryCreate(url, UriKind.Absolute, out Uri result) && (result.Scheme == Uri.UriSchemeHttp || result.Scheme == Uri.UriSchemeHttps))
+                {
+                    web.Load(textBox1.Text);
+                }
+                else
+                {
+                    web.Load("https://www.google.com/search?q=" + url.Replace(" ", "+"));
+                }
+                /*
+                char kropka = '.';              //kropka
+                int licz = url.Count(c => c == kropka); //sprawdzanie czy są dwie kropki
+                if (licz == 2)
+                {
+                    web.Load(textBox1.Text);
+                }
+                else 
+                {
+                    web.Load("https://www.google.com/search?q=" + url.Replace(" ", "+"));
+                }
+                */
+                AddDate();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser web = tabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if (web != null)
+                web.Refresh(); //odswiez
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser web = tabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if (web != null)
+            { 
+            if (web.CanGoForward)
+                web.Forward();
+            }
+            AddDate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser web = tabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if (web != null)
+            {
+                if (web.CanGoBack)
+                    web.Back();
+            }
+            AddDate();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            TabPage tab = new TabPage();
+            tab.Text = "Nowy";
+            tabControl1.Controls.Add(tab);
+            tabControl1.SelectTab(tabControl1.TabCount - 1);
+            ChromiumWebBrowser web = new ChromiumWebBrowser("https://www.interia.pl");
+            web.Parent = tab;
+            web.Dock = DockStyle.Fill;
+            textBox1.Text = "https://www.interia.pl";
+            web.AddressChanged += web_AddressChanged;
+            web.TitleChanged += web_TitleChanged;
+
+            
+        }
+
+        private void web_TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                tabControl1.SelectedTab.Text = e.Title;
+            }));
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // Wyświetlanie danych za pomocą MessageBox
+            string message = "ID\tData / Godzina\tLink do Strony\n";
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                int id = (int)row["ID"];
+                DateTime pobranaGodzina = (DateTime)row["Godzina"];
+                string linkDoStrony = (string)row["LinkDoStrony"];
+
+                message += $"{id}\t{pobranaGodzina}\t{linkDoStrony}\n";
+            }
+
+            MessageBox.Show(message, "Historia Przeglądania", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            dataTable.Clear();
+        }
+
+        private void AddDate()
+        {
+            DateTime currentDateTime = DateTime.Now;
+            dataTable.Rows.Add(currentId, currentDateTime, textBox1.Text);
+            currentId++;
+        }
+    }
+}
